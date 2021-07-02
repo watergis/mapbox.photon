571781f7f5a8facb69e9ac721c5e67260263d3ba
watergis+githubci <watergis+githubci@users.noreply.github.com>
2021-07-02 07:20:20 +0000

    deploy to gh-pages

---
+++ index.js
@@ -0,0 +1,17 @@
+import mapboxgl from 'mapbox-gl';
+import MapboxPhotonGeocoder from '../lib/index';
+import '../css/styles.css';
+
+(()=>{
+    // mapboxgl.accessToken='your mapbox access token'
+    const map = new mapboxgl.Map({
+        container: 'map',
+        // style: 'mapbox://styles/mapbox/streets-v11',
+        style:'https://narwassco.github.io/mapbox-stylefiles/unvt/style.json',
+        center: [35.87063, -1.08551],
+        zoom: 12,
+        hash:true,
+    });
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+    map.addControl(new MapboxPhotonGeocoder(), 'top-left');
+})()
